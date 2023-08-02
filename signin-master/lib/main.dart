4fe7c854687b2bf484603a4c44203794d3901c2d
HyunBeen0903 <134044125+HyunBeen0903@users.noreply.github.com>
2023-08-03 00:20:03 +0900

    Add files via upload

---
+++ signin-master/lib/main.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Login app",
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        elevation: 0.2,
+      ),
+
+    );
+  }
+}
+
